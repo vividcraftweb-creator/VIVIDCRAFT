da0700b3c4949ea2c72c4035b076d970f48605d7
vividcraftweb-creator <vividcraftweb@gmail.com>
2026-09-02 02:19:04 +0530

    add skipValidation to env schema

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   server: {
     DATABASE_URL: z.string().url(),
     NEXTAUTH_URL: z.string().url(),
